0d573e70e876a34534bb33c3a9d2ab170dfb4ecb
Your Name <you@example.com>
2015-07-30 14:36:20 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'scientists/1' => "application#einstein"
-  get 'scientists/2' => "application#marie"
-  get 'scientists/new'=>"application#new"
-  get 'scientists/:id'=> "application#show"
-  get'/create_gif'=> "application#create"
+  get 'scientists/1' => "user#einstein"
+  get 'scientists/2' => "user#marie"
+  get 'scientists/new'=>"user#new"
+  get 'scientists/:id'=> "user#show"
+  get '/scientists'=> "user#create"
+  get '/scientists/:id/edit' => 'user#edit'
+  get 'scientists'=> 'application#index'
  
 end
